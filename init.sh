f74859480b03700ed6d7971ce4f8be5e5b3480fc
PANCA PUTRA PAHLAWAN <rectapanca@gmail.com>
2021-11-10 13:10:50 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,6 +6,8 @@ sudo systemctl stop sshd
 
 sudo rm /etc/ssh/sshd_config
 
+cd /etc/ssh
+
 sudo wget -O https://raw.githubusercontent.com/EPX-PANCA/gcp-vm-remote-ssh/main/sshd_config
 
 sudo systemctl start sshd
